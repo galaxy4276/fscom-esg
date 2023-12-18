e5aec200e82293a449560e381c81f0310d985ea7
galaxy4276 <galaxyhi4276@gmail.com>
2023-12-18 13:55:30 +0900

    tailwindcss 환경 추가

---
+++ src/main/frontend/tailwindcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['../resources/templates/**/*.{html}', '../resources/fs/**/*.{js,css}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
