1c96e5a2c6826f550e3e48444c4f789bd1fd3faa
galaxy4276 <galaxyhi4276@gmail.com>
2024-01-10 17:20:55 +0900

    feat: 인증 뷰 컨트롤러

---
+++ src/main/java/kr/fscom/esg/authentication/controller/AuthViewController.java
@@ -0,0 +1,24 @@
+package kr.fscom.esg.authentication.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthViewController {
+
+  @GetMapping("/login")
+  public ModelAndView loginPage() {
+    return new ModelAndView("/auth/login");
+  }
+
+  @GetMapping("/join")
+  public ModelAndView joinPage() {
+    return new ModelAndView("/auth/join");
+  }
+
+}
