ca7284463a34896acfccc1bb560968a4bcaac4e4
galaxy4276 <galaxyhi4276@gmail.com>
2024-01-11 16:26:50 +0900

    feat: 공용 버튼 컴포넌트 작성

---
+++ src/main/resources/fs/js/components/common/button.js
@@ -0,0 +1,34 @@
+import formatter from '../../utils/formatter';
+
+/**
+ * 회원가입 버튼
+ */
+class EsgButton extends HTMLElement {
+  button = null;
+
+  connectedCallback() {
+    console.dir(this);
+    const buttonText = this.getAttribute('text');
+    const disabled = this.getAttribute('disabled');
+
+    const button = document.createElement('button');
+    this.button = button;
+    button.className = 'btn btn-slate btn-wide';
+    button.textContent = buttonText;
+    this.appendChild(button);
+    if (disabled) {
+      button.setAttribute('disabled', disabled);
+    }
+  }
+
+  static observedAttributes = ['show', 'disabled', 'text'];
+
+  attributeChangedCallback(name, oldState, newState) {
+    if (name === 'text') {
+      this.button.innerText = newState;
+    }
+  }
+
+}
+
+customElements.define('esg-button', EsgButton);
