84ece23bb05baf3bc332543f3b44652e7fb0b439
galaxy4276 <galaxyhi4276@gmail.com>
2024-01-12 14:22:30 +0900

    feat: 로그인 - UserDetails, UserDetailsService 객체 구현

---
+++ src/main/java/kr/fscom/esg/authentication/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package kr.fscom.esg.authentication.service;
+
+import kr.fscom.esg.user.domain.User;
+import kr.fscom.esg.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService  {
+  private final UserRepository userRepository;
+
+  /**
+   * @param username 사용자 이메일
+   */
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.getByEmail(username);
+    return new CustomUserDetails(user);
+  }
+
+}
