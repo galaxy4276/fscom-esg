9a37f645493206d078c91c0b65772900a91818e8
galaxy4276 <galaxyhi4276@gmail.com>
2024-01-12 10:00:47 +0900

    feat:  애플리케이션 공용 다이얼로그 컴포넌트 작성

---
+++ src/main/resources/fs/js/utils/dialog.js
@@ -0,0 +1,58 @@
+const dialogRoot = document.getElementById('dialog-root');
+
+/**
+ * 다이얼로그를 화면에 렌더링합니다.
+ * @param el {HTMLElement}
+ * @param config {{ okCb?: () => void, cancelCb: () => void, cancel: boolean } | undefined}
+ */
+export const dialogPush = (el, config = undefined) => {
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class='dialog fixed overflow-auto w-full h-full left-0 top-0 flex justify-center items-center z-50'>
+        <div id='dialog-background' class="absolute top-0 left-0 z-50 flex layout-full-center">
+           <div class="esg-dialog-box">
+             <div class='esg-dialog__content z-[100]'>
+            </div>
+              <div class='esg-dialog__control flex gap-x-4 justify-center self-end'>
+              <esg-button id="ok" text="확인했어요"></esg-button>
+              <esg-button id="cancel" text="취소할게요" style="display: none"></esg-button>
+            </div>
+          </div>
+        </div>
+      </div>    
+    </div>
+  `;
+
+  const content = div.querySelector('.esg-dialog__content');
+  content.style.boxShadow = '0px 4px 24px 0px rgba(0, 0, 0, 0.15)';
+  const okBtn = div.querySelector('#ok');
+  const cancelBtn = div.querySelector('#cancel');
+  const background = div.querySelector('#dialog-background');
+  background.style.backgroundColor = 'rgb(71,85,105,0.4)';
+
+  content.appendChild(el);
+  dialogRoot.appendChild(div);
+
+  okBtn.onclick = () => {
+    if (config?.okCb) {
+      config.okCb();
+    }
+    div.remove();
+  };
+
+  if (config) {
+
+    if (config.cancel) {
+      cancelBtn.style.display = 'block';
+      cancelBtn.onclick = () => {
+        if (config?.cancelCb) {
+          config.cancelCb();
+        }
+        div.remove();
+      };
+    }
+
+  }
+
+
+};
