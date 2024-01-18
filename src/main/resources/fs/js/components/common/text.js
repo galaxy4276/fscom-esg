2ccf263fe7f14e1840858db7edccef436cf89bdc
galaxy4276 <galaxyhi4276@gmail.com>
2024-01-18 19:35:13 +0900

    feat: 공용 텍스트 컴포넌트 작성

---
+++ src/main/resources/fs/js/components/common/text.js
@@ -0,0 +1,60 @@
+class Text extends HTMLElement {
+
+  connectedCallback() {
+    const text = this.getAttribute('text');
+    const sizeProp = this.getAttribute('size');
+    const boldProp = this.getAttribute('bold');
+    const colorProp = this.getAttribute('color');
+    const hrefProp = this.getAttribute('href');
+
+    this.innerHTML = `
+      <span>${text}</span>
+    `
+
+    const element = this.querySelector('span');
+    console.dir(element);
+    element.style.fontSize = this.getSize(sizeProp);
+    if (boldProp === 'true') {
+      element.style.fontWeight = '600';
+    }
+
+    element.style.color = colors[colorProp];
+
+    if (hrefProp) {
+      element.style.cursor = 'pointer';
+      element.onclick = () => location.href = hrefProp
+    }
+
+  }
+
+  getSize(s) {
+    switch (s) {
+      case 'small': {
+        return '14px';
+      }
+      case 'normal': {
+        return '16px';
+      }
+      case 'medium': {
+        return '18px';
+      }
+      case 'large': {
+        return '20px';
+      }
+      default: {
+        return '14px';
+      }
+    }
+  }
+
+}
+
+const colors = {
+  title: '#334155',
+  paragraph: '#64748B',
+}
+
+
+customElements.define("esg-text", Text)
+
+export default Text
