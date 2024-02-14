6e34eaedb8c7e43caa22afb95cb6d5aae46c6d2b
galaxy4276 <galaxyhi4276@gmail.com>
2024-02-14 17:31:26 +0900

    feat: 헤더 드랍다운 제어 로직 위치 이관

--- src/main/resources/fs/js/components/header/index.js
+++ src/main/resources/fs/js/components/header/index.js
@@ -131,3 +131,65 @@ drawerOpenCloseButton.addEventListener('click', () => {
 
   userIdentity.textContent = `${user.name}님 환영합니다`;
 })();
+
+/**
+ * @variation 헤더 드롭다운 제어
+ */
+(() => {
+  const topNavMenus = [
+    // 소개
+    [document.getElementById('top-nav__dropdown--content-intro'), document.getElementById('top-nav__dropdown--intro')],
+
+    // 평가
+    [document.getElementById('top-nav__dropdown--content-eval'), document.getElementById('top-nav__dropdown--eval')],
+
+    // 사업
+    [document.getElementById('top-nav__dropdown--content-work'), document.getElementById('top-nav__dropdown--work')],
+  ];
+
+  topNavMenus.forEach(([content, dropdown]) => {
+    let introEntered = false;
+    let visitContent = false;
+
+    const enter = () => {
+      introEntered = true;
+    };
+
+    const leave = () => {
+      introEntered = false;
+      content.style.visibility = 'hidden';
+    };
+
+    dropdown.addEventListener('mouseover', () => {
+      enter();
+      content.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', (e) => {
+      console.log('dropdown mouseleave');
+      if (!introEntered) {
+        leave();
+      }
+
+      if (!visitContent) {
+        leave();
+      }
+    });
+
+    // dropdown content mouse event
+    content.addEventListener('mouseenter', (e) => {
+      visitContent = true;
+      const mouseOverEv = new MouseEvent('mouseover', { bubbles: false });
+      dropdown.dispatchEvent(mouseOverEv);
+      console.log('content entered');
+      enter();
+    });
+
+    content.addEventListener('mouseleave', (e) => {
+      e.stopPropagation();
+      visitContent = false;
+      console.log('content leave');
+      leave();
+    });
+  });
+})()
