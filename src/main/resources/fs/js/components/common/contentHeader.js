ddbad82e81cb066a91241afa30e698c5e8090ec5
galaxy4276 <galaxyhi4276@gmail.com>
2024-02-21 22:23:32 +0900

    feat: 레이아웃 개선

--- src/main/resources/fs/js/components/common/contentHeader.js
+++ src/main/resources/fs/js/components/common/contentHeader.js
@@ -2,6 +2,8 @@ export class ContentHeader extends HTMLElement {
   connectedCallback() {
     const title = this.getAttribute("title")
 
+    this.style.width = "100%"
+
     this.innerHTML = `
        <div class="w-full forest-bg">
       <div class="flex flex-col justify-center mx-0 lg:mx-[240px] animate__animated animate__fadeIn">
