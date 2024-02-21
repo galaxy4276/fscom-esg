18657c9cfa6b2106fd7f7616ea3df0a10e2464d8
galaxy4276 <galaxyhi4276@gmail.com>
2024-02-22 00:10:15 +0900

    feat: 카드 컴포넌트 디자인

---
+++ src/main/resources/fs/js/components/common/articleCard.js
@@ -0,0 +1,25 @@
+import dayjs from 'dayjs';
+
+export class ArticleCard extends HTMLElement {
+  connectedCallback() {
+    const banner = this.getAttribute('src');
+    const rawTitle = this.getAttribute('title');
+    const title = rawTitle.length >= 38 ? rawTitle.substring(0, 38) + "..." : rawTitle;
+    const rawCreatedDate = this.getAttribute('created-date');
+    const createdDate = dayjs(rawCreatedDate).format('YYYY.MM.DD');
+
+    this.innerHTML = `
+      <div class="card shadow-lg w-full md:max-w-[280px]">
+        <div class="card-body">
+          <h2 class="card-title">${title}</h2>
+          <span class="text-slate-500">${createdDate}</span>
+        </div>
+         <figure>
+            <img src="${banner}"> 
+        </figure>
+      </div>
+    `
+  }
+}
+
+customElements.define("article-card", ArticleCard)
