044d39e6c6f43871c636ab986b8a653bde55c6df
galaxy4276 <galaxyhi4276@gmail.com>
2024-02-26 21:50:03 +0900

    feat: 간단한 게시글 작성 레이아웃

--- src/main/resources/fs/js/page/editor.js
+++ src/main/resources/fs/js/page/editor.js
@@ -2,13 +2,20 @@ import * as Quill from "quill"
 
 const editorElm = document.getElementById('editor');
 
-
 (() => {
   if (!editorElm) return;
   const editor = new Quill('#editor', {
     theme: 'snow',
   });
   console.log({ editor });
+
+  const previousBtns = Array.from(document.getElementsByClassName('previous-btn'));
+  previousBtns.forEach(b => {
+    b.addEventListener('click', e => {
+      e.preventDefault();
+      history.back();
+    });
+  });
 })();
 
 (() => {
