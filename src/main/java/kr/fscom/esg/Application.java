e7a69bb773cd0801fb70de100b764bfaffc399bc
galaxy4276 <galaxyhi4276@gmail.com>
2024-02-28 11:08:53 +0900

    feat: 애플리케이션 실행 시 파일 업로드 보관 디렉터리 자동 생성

--- src/main/java/kr/fscom/esg/Application.java
+++ src/main/java/kr/fscom/esg/Application.java
@@ -1,5 +1,9 @@
 package kr.fscom.esg;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,16 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct
+	public void createResourceFile() {
+		try {
+			Files.createDirectory(Paths.get("src/main/resources/uploads"));
+		} catch (IOException ex) {
+			System.out.println("업로드 파일 생성 중 오류가 발생하였습니다.");
+			ex.printStackTrace();
+		}
 	}
 
 }
