9879e28545e54b9dd880756285e76f2da690b66e
galaxy4276 <galaxyhi4276@gmail.com>
2024-03-14 23:36:08 +0900

    feat: 게시글 작성자 인덱스 키 부여

--- src/main/java/kr/fscom/esg/post/service/PostCrudService.java
+++ src/main/java/kr/fscom/esg/post/service/PostCrudService.java
@@ -2,11 +2,15 @@ package kr.fscom.esg.post.service;
 
 import java.awt.print.Pageable;
 import java.util.List;
+import kr.fscom.esg.authentication.service.CustomUserDetails;
 import kr.fscom.esg.post.domain.PostCategory;
 import kr.fscom.esg.post.domain.PostCreation;
 import kr.fscom.esg.post.domain.PostCreationRequest;
 import kr.fscom.esg.post.domain.PostSummary;
 import kr.fscom.esg.post.repository.PostMapper;
+import kr.fscom.esg.user.domain.User;
+import kr.fscom.esg.user.repository.UserMapper;
+import kr.fscom.esg.user.service.UserGetterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -19,12 +23,15 @@ import org.springframework.stereotype.Service;
 public class PostCrudService {
 
   private final PostMapper postMapper;
+  private final UserGetterService userGetterService;
 
   public void create(PostCreationRequest request) {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    Long userId = Long.getLong((String) authentication.getPrincipal());
-    log.info("userId: {}", userId);
-    PostCreation creation = PostCreation.from(request, userId);
+    CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+    String email = principal.getUsername();
+    User user = userGetterService.getUser(email);
+    log.info("userId: {}", user.getId());
+    PostCreation creation = PostCreation.from(request, user.getId());
     postMapper.createPost(creation);
   }
 
