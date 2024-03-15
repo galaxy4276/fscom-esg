bfa1dd1f1da680bce4e2eacfb63abef4025708d9
galaxy4276 <galaxyhi4276@gmail.com>
2024-03-15 09:48:41 +0900

    doc: api 문서 정리

--- src/main/java/kr/fscom/esg/file/controller/FileController.java
+++ src/main/java/kr/fscom/esg/file/controller/FileController.java
@@ -1,6 +1,7 @@
 package kr.fscom.esg.file.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.fscom.esg.file.domain.EsgFile;
 import kr.fscom.esg.file.service.FileStorageHandler;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "파일 업로드 API")
 @RestController
 @RequestMapping("/file")
 @RequiredArgsConstructor
