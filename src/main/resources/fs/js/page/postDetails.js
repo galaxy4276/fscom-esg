a1e6c972798d4ef17f1383dfd2679ad44313e95e
galaxy4276 <galaxyhi4276@gmail.com>
2024-03-17 20:17:59 +0900

    feat: 게시글 제목, 작성일 조회

--- src/main/resources/fs/js/page/postDetails.js
+++ src/main/resources/fs/js/page/postDetails.js
@@ -1,6 +1,7 @@
 import pageHook from '../utils/pageHook';
 import { PostService } from '../utils/api';
 import { pushErrorDialog } from '../utils/dialog';
+import domUtils from '../utils/dom';
 
 const getPostDetails = async (id) => {
   try {
@@ -12,11 +13,15 @@ const getPostDetails = async (id) => {
 };
 
 pageHook(['/post/'], async () => {
-  console.log(location.href);
+  const titleElm = document.getElementById('postTitle');
+  const dateElm = document.getElementById('postDate');
+
   const id = (() => {
     const tokens = location.href.split('/');
     return Number.parseInt(tokens[tokens.length - 1]);
   })();
   const post = await getPostDetails(id);
   console.log({ post });
+  domUtils.setEsgText(titleElm, post.title);
+  domUtils.setEsgText(dateElm, post.createdAt);
 });
---
+++ src/main/resources/fs/js/utils/dom.js
@@ -0,0 +1,7 @@
+const domUtils = {
+  setEsgText: (el, text) => {
+    el.setAttribute('text', text);
+  },
+};
+
+export default domUtils;
