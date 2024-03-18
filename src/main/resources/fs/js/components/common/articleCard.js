e9436edbadeb8e316a1b7c18e8ef93643e4c652d
galaxy4276 <galaxyhi4276@gmail.com>
2024-03-18 15:37:28 +0900

    fix: 대표 이미지 버그 수정

--- src/main/resources/fs/js/components/common/articleCard.js
+++ src/main/resources/fs/js/components/common/articleCard.js
@@ -14,11 +14,11 @@ export class ArticleCard extends HTMLElement {
           <h2 class="card-title">${title}</h2>
           <span class="text-slate-500">${createdDate}</span>
         </div>
-         ${banner && `
+         ${banner ? `
            <figure>
               <img src="${banner}"> 
           </figure>
-         `}
+         ` : ''}
       </div>
     `
   }
