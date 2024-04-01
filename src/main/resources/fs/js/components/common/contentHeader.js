f25531579236d4da1592ed9e7e47309956a07d26
galaxy4276 <galaxyhi4276@gmail.com>
2024-04-02 05:13:07 +0900

    fix: 헤더 크기 조정 (padding y)

--- src/main/resources/fs/js/components/common/contentHeader.js
+++ src/main/resources/fs/js/components/common/contentHeader.js
@@ -7,7 +7,7 @@ export class ContentHeader extends HTMLElement {
     this.innerHTML = `
        <div class="w-full forest-bg">
       <div class="flex flex-col justify-center mx-0 lg:mx-[240px] animate__animated animate__fadeIn">
-        <div class="flex flex-col items-center pt-[120px] py-[180px] whitespace-nowrap">
+        <div class="flex flex-col items-center pt-[70px] py-[50px] whitespace-nowrap">
           <p class="text-white text-[52px] md:text-[72px] font-bold">${title}</p>
         </div>
       </div>
